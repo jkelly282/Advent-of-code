80946dd7b6245e516fb814ce2b7b35e4e3ca73d1
jkelly2 <jkelly2@illumina.com>
2019-02-13 16:27:57 +0000

    the thing

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -2,13 +2,12 @@ import unittest
 
 from pyfakefs.fake_filesystem_unittest import Patcher
 
-from day1.day1 import open_file
+from day1 import open_file
 
 
 class TestOpenFile(unittest.TestCase):
     def setUp(self):
         self.test_file = 'test.txt'
-
 
     def test_open_file_success(self):
         expected = [1, -2, 5]
@@ -16,13 +15,11 @@ class TestOpenFile(unittest.TestCase):
             patch.fs.create_file(self.test_file, contents="+1\n-2\n+5\n")
             self.assertEquals(expected, open_file(self.test_file))
 
-
     def test_open_file_fail(self):
         expected = [1, 3, 5]
         with Patcher() as patch:
             patch.fs.create_file(self.test_file, contents="+1\n-2\n+5\n")
             self.assertNotEquals(expected, open_file(self.test_file))
-
 
     def test_open_file_throws_exception(self):
         with Patcher() as patch:
