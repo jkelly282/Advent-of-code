1356fef4e1dde5a588c8a9808e21f2626d3f8171
jkelly2 <jkelly2@illumina.com>
2019-02-14 10:56:42 +0000

    Updated with the lessons learnt yesterday

--- day2/Day 2.py
+++ day2/Day 2.py
@@ -1,24 +1,44 @@
-# Storing the letters of the alphabet into a variable
-chars = "abcdefghijklmnopqrstuvwxyz"
-# Open the text file not a generic name which python already uses
-text_file = open("/Users/jkelly2/Documents/testcheck.txt", "r")
-# intitialising the variables for how many repeats
-two = 0
-three = 0  # type: int
+import argparse
 
-for i in range(0, 10000):
-    string = text_file.readline()
-    two_check = 0  # type: int
-    three_check = 0
-    for char in chars:
-        count = string.count(char)
-        if count > 1:
+def open_file(filepath):
+    with open(filepath) as f:
+        return [str(item.strip()) for item in f]
 
-            if count == 2 and two_check == 0:
-                two += 1
-                two_check += 1
-            elif count == 3 and three_check == 0:
-                three += 1
-                three_check += 1
-checksum = two * three
-print(checksum)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Day 2 refactoring")
+    parser.add_argument('-f', dest='text_strings', required=True, help="Newline seperated text file")
+    args = parser.parse_args()
+    return args
+
+
+def find_checksum():
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    two = 0
+    three = 0  # type: int
+    for i in mylines:
+        two_check = 0  # type: int
+        three_check = 0
+        for char in chars:
+            count = i.count(char)
+            if count > 1:
+                if count == 2 and two_check == 0:
+                    two += 1
+                    two_check += 1
+                elif count == 3 and three_check == 0:
+                    three += 1
+                    three_check += 1
+    checksum = two * three
+    return checksum
+
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    try:
+        mylines = open_file(args.text_strings)
+    except ValueError:
+        logging.error("File should contain lines")
+    checksum = find_checksum()
+    print(checksum)
