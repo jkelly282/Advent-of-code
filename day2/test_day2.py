ab47d4dc7386b186ad0b609fa6b2ccae19532c0e
jkelly2 <jkelly2@illumina.com>
2019-02-19 14:34:32 +0000

    Added in fail tests for similar strings and remove duplicates

--- day2/test_day2.py
+++ day2/test_day2.py
@@ -27,10 +27,20 @@ class Test_day_modules(unittest.TestCase):
         mylines = ["aaaaaaaaab", "aaaaaaaaaa", "dkoskdkdakr"]
         self.assertEqual(expected, find_similar_strings(mylines))
 
+    def test_find_similar_Strings_fail(self):
+        expected = ("aaaaaaaaaa", "aaaaaaaaab")
+        mylines = ("I wanna be the very best like no-one ever was ")
+        self.assertNotEqual(expected, find_similar_strings(mylines))
+
     def test_remove_duplicates(self):
         expected = ['a'] * 9
         test = ("aaaaaaaaaa", "aaaaaaaaab")
         self.assertEqual(expected, remove_duplicates(test))
+
+    def remove_duplicates_fail(self):
+        expected = ['a'] * 9
+        test = ("To catch them is my real goal, to train them is my cause")
+        self.assertNotEqual(expected, find_similar_strings(test))
 
 
 if __name__ == '__main__':
