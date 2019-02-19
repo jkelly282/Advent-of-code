336be3349701c0e3d2f25dc3f509e15155abeb2e
jkelly2 <jkelly2@illumina.com>
2019-02-19 09:47:07 +0000

    added test^

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -13,19 +13,21 @@ class TestOpenFile(unittest.TestCase):
         expected = [1, -2, 5]
         with Patcher() as patch:
             patch.fs.create_file(self.test_file, contents="+1\n-2\n+5\n")
-            self.assertEquals(expected, open_file(self.test_file))
+            self.assertEqual(expected, open_file(self.test_file))
 
     def test_open_file_fail(self):
         expected = [1, 3, 5]
         with Patcher() as patch:
             patch.fs.create_file(self.test_file, contents="+1\n-2\n+5\n")
-            self.assertNotEquals(expected, open_file(self.test_file))
+            self.assertNotEqual(expected, open_file(self.test_file))
 
     def test_open_file_throws_exception(self):
         with Patcher() as patch:
             patch.fs.create_file(self.test_file, contents="This should really fail")
             self.assertRaises(ValueError, open_file, self.test_file)
 
+    def test_find_duplicate(self):
+        self.skipTest('laziness')
 
 if __name__ == '__main__':
     unittest.main()
