31bb09fdc0946862a2886df552310d13c2afcd1d
jkelly2 <jkelly2@illumina.com>
2019-02-19 13:09:30 +0000

    Changed import into function

--- day2/Day 2.py
+++ day2/Day 2.py
@@ -16,11 +16,11 @@ def parse_args():
     return args
 
 
-def find_checksum():
+def find_checksum(checksum_list):
     chars = "abcdefghijklmnopqrstuvwxyz"
     two = 0
     three = 0  # type: int
-    for i in mylines:
+    for i in checksum_list:
         two_check = 0  # type: int
         three_check = 0
         for char in chars:
@@ -32,6 +32,7 @@ def find_checksum():
                 elif count == 3 and three_check == 0:
                     three += 1
                     three_check += 1
+
     checksum = two * three
     return checksum
 
@@ -47,10 +48,10 @@ def find_similar_strings():
             if ratio_matching == percentage_correct:
                 return mylines[j], mylines[i]
 
-def remove_duplicates(duplicates):
-    answer= [i for i, j in zip(*duplicates) if i == j]
-    return(answer)
 
+def remove_duplicates(duplicates):
+    answer = [i for i, j in zip(*duplicates) if i == j]
+    return (answer)
 
 
 if __name__ == '__main__':
@@ -59,7 +60,7 @@ if __name__ == '__main__':
         mylines = open_file(args.text_strings)
     except ValueError:
         logging.error("File should contain lines")
-    checksum = find_checksum()
+    checksum = find_checksum(mylines)
     print(checksum)
     test = find_similar_strings()
     answer = remove_duplicates(test)
