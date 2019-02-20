f612fe97625c934b3b85ac7bdc731c83692520a3
jkelly2 <jkelly2@illumina.com>
2019-02-20 13:25:55 +0000

    Added unit tests for the first function - it looks like it works as expected

--- day3/test_day3.py
+++ day3/test_day3.py
@@ -12,6 +12,20 @@ class TestDay3(unittest.TestCase):
         mylines = ["#1 @ 1,2: 3x4"]
         self.assertEqual(expected, day3.parse_claims(mylines))
 
+    def test_parse_claims_fail(self):
+        expected = [['1', '2', '3', '4']]
+        mylines = ["1234"]
+        self.assertNotEqual(expected, day3.parse_claims(mylines))
+
+    def test_parse_claims_type(self):
+        mylines = 43770
+        self.assertRaises(TypeError, day3.parse_claims, mylines)
+
+    def test_parse_claims_empty(self):
+        expected = [['1', '2', '3', '4']]
+        mylines = [""]
+        self.assertNotEqual(expected, day3.parse_claims(mylines))
+
 
 if __name__ == '__main__':
     unittest.main()
