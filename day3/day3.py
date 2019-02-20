8758f6857ee2dd349c7075f8edac025b1f0355c2
jkelly2 <jkelly2@illumina.com>
2019-02-20 11:40:37 +0000

    Added new function parse_claims - tests to follow

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,17 @@
 import logging
+import re
 
 import day2
+
+
+def parse_claims(mylines):
+    fabric_coordinates = []
+    for i in mylines:
+        parsed_claim = re.split('@|,|:|x', i)
+        fabric_coordinates.append(parsed_claim)
+    return fabric_coordinates
+
+
 
 if __name__ == '__main__':
     args = day2.parse_args()
