0da66d92d9f6bd2bf98cef8f8f28cc70856920b4
jkelly2 <jkellyis@yahoo.co.uk>
2019-05-08 21:13:50 +0100

    hack

--- day4/day4.py
+++ day4/day4.py
@@ -1,9 +1,9 @@
 import logging
 from datetime import datetime
-
 import pandas as pd
+from day2 import day2
 
-import day2
+()
 
 
 def time_parse(line):
@@ -11,6 +11,10 @@ def time_parse(line):
     map(int, time)
     return time
 
+def time_parse_minute(line):
+    time = line[15:17]
+    map(int, time)
+    return(int(time))
 
 def parse_guard_name(line):
     guard_id = line[26:30]
@@ -22,27 +26,82 @@ def days_between(d1, d2):
     d2 = datetime.strptime(d2, "%Y-%m-%d %H:%M")
     return d2 - d1
 
+def find_most_frequent(lines, guard):
+    guard_number = ""
+    minutes_asleep = []
+    d1 = 0
+    d2 = 0
+    for line in lines:
+        if '#' in line:
+            guard_number = parse_guard_name(line)
+        if guard_number == guard:
+            print(line)
+            if "falls" in line:
+                d1 = time_parse_minute(line)
+            if "wakes" in line:
+                d2 = time_parse_minute(line)
+                minutes_asleep += range(d1,d2)
+                print(minutes_asleep)
+    minutes_asleep_dict = {}
+    for i in minutes_asleep:
+        if i in minutes_asleep_dict:
+            minutes_asleep_dict[i] += 1
+        if i not in minutes_asleep_dict:
+            minutes_asleep_dict[i] = 1
+        sorted_minutes = sorted(minutes_asleep_dict.items(), key=lambda kv: kv[1], reverse=True)
+        print(sorted_minutes)
+        minute_winning = sorted_minutes[0][0]
+        answer = int(minute_winning)*int(guard[0:2])
+        print(answer)
 
-def parse_list_chronological_date():
-    guard_shifts = pd.read_csv("guard_shifts.csv", sep="\t", names=["Time", "Activity"])
-    pd.period_range('1516-01-01', '1518-12-31', freq='D')
-    guard_shifts["Time"] = pd.to_datetime(guard_shifts["Time"], format="[%Y-%m-%d %H:%M]")
-    guard_shifts.sort_values('Time', inplace=True)
-    for line in guard_shifts:
-        print("hello"
-        dea)
-        # print(guard_shifts)
+
+def find_most_frequent_minute(lines):
+    guard_number = ""
+    minutes_asleep = []
+    d1 = 0
+    d2 = 0
+    for line in lines:
+        if '#' in line:
+            guard_number = parse_guard_name(line)
+        if guard_number == guard:
+            print(line)
+            if "falls" in line:
+                d1 = time_parse_minute(line)
+            if "wakes" in line:
+                d2 = time_parse_minute(line)
+                minutes_asleep += range(d1,d2)
+                print(minutes_asleep)
+    minutes_asleep_dict = {}
+    for i in minutes_asleep:
+        if i in minutes_asleep_dict:
+            minutes_asleep_dict[i] += 1
+        if i not in minutes_asleep_dict:
+            minutes_asleep_dict[i] = 1
+        sorted_minutes = sorted(minutes_asleep_dict.items(), key=lambda kv: kv[1], reverse=True)
+        print(sorted_minutes)
+        minute_winning = sorted_minutes[0][0]
+        answer = int(minute_winning)*int(guard[0:2])
+
+        print(guard_number)
+        print((sorted_minutes[0][0]*int(guard_number[0:3])))
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-
     args = day2.parse_args()
     try:
         mylines = day2.open_file(args.text_strings)
     except ValueError:
         logging.error("File should contain lines")
     mylines.sort()
-    parse_list_chronological_date()
+
     guard_id = {}
     d1 = 0
     d2 = 0
@@ -63,4 +122,6 @@ if __name__ == '__main__':
             guard_id[guard_number] += int((sleep.total_seconds() / 60))
     # print(guard_id)
     sorted_guard = sorted(guard_id.items(), key=lambda kv: kv[1], reverse=True)
-    print(sorted_guard)
+    winning = (sorted_guard[0][0])
+    find_most_frequent(mylines, winning)
+    find_most_frequent_minute(mylines)
